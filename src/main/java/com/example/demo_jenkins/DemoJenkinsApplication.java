6bd306762f0f619811294ee6fbdf94548ebe4470
powb <nguyensybao1403@gmail.com>
2025-06-25 10:58:51 +0700

    add logging

--- src/main/java/com/example/demo_jenkins/DemoJenkinsApplication.java
+++ src/main/java/com/example/demo_jenkins/DemoJenkinsApplication.java
@@ -3,10 +3,20 @@ package com.example.demo_jenkins;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.log4j.Log4j2;
+
 @SpringBootApplication
+@Log4j2
 public class DemoJenkinsApplication {
 
+	@PostConstruct
+	public void init() {
+		log.info("Application Started...");
+	}
+
 	public static void main(String[] args) {
+		log.info("Application executed...");
 		SpringApplication.run(DemoJenkinsApplication.class, args);
 	}
 
--- src/test/java/com/example/demo_jenkins/DemoJenkinsApplicationTests.java
+++ src/test/java/com/example/demo_jenkins/DemoJenkinsApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.demo_jenkins;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @SpringBootTest
 class DemoJenkinsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("Test case executing...");
+		assertEquals(true, true);
 	}
 
 }
